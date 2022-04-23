0fac3874fa3a9586fd0fd8407001ea279e36629e
Eric Voirin <oerg866@tototek.com>
2022-04-23 03:13:27 +0200

    a_wvout: Rework waveout backend for universal OS compatibility.
    Throws out the ringbuffer and does its own thing.
    
    Tested on 29 years worth of OSs :P

--- audio/a_wvout.c
+++ audio/a_wvout.c
@@ -5,13 +5,13 @@
 #ifdef AUDIO_WAVEOUT
 
 /*
- *  Audio Backend Implementation for Win16/Win32 WAVEOUT API
+ *  Audio Backend Implementation for Win32 WAVEOUT API
  */
 
 #include <mmsystem.h>
 
-#ifdef __WIN16__
-typedef WAVEFORMAT WAVEFORMAT_T;
+#ifndef WAVEFORMAT
+typedef PCMWAVEFORMAT WAVEFORMAT_T;
 #else
 typedef WAVEFORMATEX WAVEFORMAT_T;
 #endif
@@ -21,37 +21,50 @@ typedef WAVEFORMATEX WAVEFORMAT_T;
 #include "ringbuf.h"
 #include "trace.h"
 
-static HWAVEOUT s_waveout_handle = NULL;
-static WAVEHDR *s_wavehdrs = NULL;
-static ringbuffer *s_ringbuf = NULL;;
 
 #define BUFFER_COUNT 4
 #define BUFFER_LENGTH_MINIMUM 1536
 
 
-void a_waveout_cfg_to_waveformat(audio_config *cfg, WAVEFORMAT_T *waveformat) {
+static HWAVEOUT s_waveout_handle = NULL;
+static WAVEHDR *s_wavehdrs = NULL;
+
+static uint8_t *s_buffer_pool = NULL;
+
+static int s_write_index = 0;
+static int s_write_pos = 0;
+
+static uint32_t s_buffer_size;
+
+static CRITICAL_SECTION s_lock;
+
+void a_waveout_cfg_to_waveformat(audio_config *cfg, WAVEFORMAT_T *out) {
+#ifdef WAVEFORMATEX
+    WAVEFORMAT_T *waveformat = out;
+    waveformat->wBitsPerSample = cfg->bits_per_sample;
+    waveformat->cbSize = 0;
+#else
+    // Win16 & Early Win32, use PCMWAVEFORMAT
+    WAVEFORMAT *waveformat = &out->wf;
+    out->wBitsPerSample = cfg->bits_per_sample;
+#endif
     waveformat->wFormatTag = WAVE_FORMAT_PCM;
     waveformat->nChannels = cfg->channels;
     waveformat->nSamplesPerSec = cfg->sample_rate;
     waveformat->nAvgBytesPerSec = cfg->sample_rate * (cfg->bits_per_sample / 8) * cfg->channels;
     waveformat->nBlockAlign = (cfg->bits_per_sample / 8) *cfg->channels;
-#ifndef __WIN16__
-    waveformat->wBitsPerSample = cfg->bits_per_sample;
-    waveformat->cbSize = 0;
-#endif
 }
 
 void CALLBACK a_waveout_callback (HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
     WAVEHDR *header;
-    unsigned bufferNum;
 
     if (uMsg == WOM_DONE) {
+
+        EnterCriticalSection(&s_lock);
         header = (WAVEHDR*) dwParam1;
-        bufferNum = header->dwUser;
-        // This call is ommited because the header never change, seems to work fine:
-        // waveOutUnprepareHeader(hwo, (WAVEHDR*) header, sizeof(WAVEHDR));
-        ringbuffer_unblock(s_ringbuf, bufferNum);
-        waveOutWrite(hwo, &s_wavehdrs[ringbuffer_increment_fetch_and_block(s_ringbuf)], sizeof(WAVEHDR));
+        header->dwFlags = WHDR_PREPARED;
+        LeaveCriticalSection(&s_lock);
+
     }
 }
 
@@ -59,8 +72,9 @@ int a_waveout_init(audio_config *cfg) {
     WAVEFORMAT_T waveformat;
     MMRESULT ret;
     uint32_t bytes_per_sample = cfg->bits_per_sample / 8;
-    uint32_t ringbuffer_slice_size;
     uint32_t i;
+
+    InitializeCriticalSection(&s_lock);
 
     memset(&waveformat, 0, sizeof(WAVEFORMAT_T));
 
@@ -72,13 +86,15 @@ int a_waveout_init(audio_config *cfg) {
         print_msg("WARNING: Buffer size extremely low, increasing\n");
     }
 
-    ringbuffer_slice_size = cfg->buffer_size * cfg->channels * bytes_per_sample;
-
     // Build WAVEFORMAT(EX) structure out of the audio config.
     a_waveout_cfg_to_waveformat(cfg, &waveformat);
 
+    s_buffer_size = cfg->buffer_size;
+
+    s_buffer_pool = alloc_mem(s_buffer_size * BUFFER_COUNT);
+
     // Allocate memory for ringbuffer.
-    s_ringbuf = ringbuffer_create(ringbuffer_slice_size, BUFFER_COUNT);
+
     s_wavehdrs = alloc_mem(BUFFER_COUNT * sizeof(WAVEHDR));
     memset(s_wavehdrs, 0, BUFFER_COUNT * sizeof(WAVEHDR));
 
@@ -86,19 +102,23 @@ int a_waveout_init(audio_config *cfg) {
     ret = waveOutOpen(&s_waveout_handle, WAVE_MAPPER, &waveformat, (DWORD_PTR) a_waveout_callback, 0, CALLBACK_FUNCTION);
     print_msg("MMRESULT = %d\n", ret);
 
+    s_write_index = 0;
+
     // Initialize and prepare headers
+    // Give it a little head start because WAVEOUT is slow as hell...
+
     for (i = 0; i < BUFFER_COUNT; ++i) {
-        s_wavehdrs[i].dwBufferLength = ringbuffer_slice_size;
-        s_wavehdrs[i].lpData = (char*) ringbuffer_get_data(s_ringbuf, i);
+        s_wavehdrs[i].dwBufferLength = s_buffer_size;
+        s_wavehdrs[i].lpData = &s_buffer_pool[s_buffer_size * i];
         s_wavehdrs[i].dwUser = i;
         waveOutPrepareHeader(s_waveout_handle, &s_wavehdrs[i], sizeof(WAVEHDR));
     }
 
-    // Give it a little head start because WAVEOUT is slow as hell...
-    for (i = 0; i < BUFFER_COUNT/2; ++i) {
-        ringbuffer_increment_fetch_and_block(s_ringbuf);
+    for (i = 0; i < BUFFER_COUNT; ++i) {
+
         ret = waveOutWrite(s_waveout_handle, &s_wavehdrs[i], sizeof(WAVEHDR));
         print_msg("MMRESULT = %d\n", ret);
+
     }
 
     print_msg("WaveOut init done\n");
@@ -106,13 +126,63 @@ int a_waveout_init(audio_config *cfg) {
 }
 
 void a_waveout_deinit(void) {
+    unsigned i;
+
     waveOutClose(s_waveout_handle);
+    for (i = 0; i < BUFFER_COUNT; ++i) {
+        waveOutUnprepareHeader(s_waveout_handle, &s_wavehdrs[i], sizeof(WAVEHDR));
+    }
     free(s_wavehdrs);
-    ringbuffer_destroy(s_ringbuf);
+    free(s_buffer_pool);
+    DeleteCriticalSection(&s_lock);
 }
 
 audio_buffer_status a_waveout_play_buffer(uint8_t *buffer, uint32_t length) {
-    ringbuffer_insert_bytes(s_ringbuf, buffer, length);
+    WAVEHDR *header = NULL;
+    MMRESULT ret = 0;
+    volatile int r = 0;
+    int w = 0;
+    uint8_t *dst = NULL;
+    uint8_t *src = buffer;
+    uint32_t bytesToCopy = 0;
+    uint32_t free_space;
+    DWORD tmp = 0;
+
+    while (length) {
+
+        header = &s_wavehdrs[s_write_index];
+
+        // Wait until next buffer is free
+
+        do {
+            EnterCriticalSection(&s_lock);
+            tmp = header->dwFlags;
+            LeaveCriticalSection(&s_lock);
+            yield();
+        } while (tmp & WHDR_PREPARED == 0);
+
+
+        // Get bytes left in this buffer.
+
+        dst = &s_buffer_pool[s_write_index * s_buffer_size + s_write_pos];
+        free_space = s_buffer_size-s_write_pos;
+
+        if (length < free_space) {
+            memcpy (dst, src, length);
+            s_write_pos += length;
+            break;
+        } else {
+            memcpy (dst, src, free_space);
+            header = &s_wavehdrs[s_write_index];
+            ret = waveOutWrite(s_waveout_handle, header, sizeof(WAVEHDR));
+            length -= free_space;
+            src += free_space;
+            s_write_pos = 0;
+            s_write_index = (s_write_index + 1) % BUFFER_COUNT;
+            if (length == 0) break;
+        }
+
+    }
     return AUDIO_BUFFER_COPIED;
 }
 
