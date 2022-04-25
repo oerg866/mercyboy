a042524a6c885a94da927ecbe2f989ecd26fe8ab
Eric Voirin <oerg866@tototek.com>
2022-04-26 01:10:23 +0200

    v_dosvga: Center scree + add code to restore old video mode and palette.

--- video/v_dosvga.c
+++ video/v_dosvga.c
@@ -2,9 +2,7 @@
 
 #define NAME "v_dosvga"
 
-// #ifdef VIDEO_DOSVGA
-
-#if (1)
+#ifdef VIDEO_DOSVGA
 
 /*
  *  Video Backend Implementation for MS-DOS Mode 13h VGA
@@ -33,41 +31,97 @@ static const vgacolor bw_palette[4] = { { 0x3F, 0x3F, 0x3F },
                                         { 0x15, 0x15, 0x15 },
                                         { 0x00, 0x00, 0x00 } };
 
+// we overwrite 4*3 + 1 colors, this is the previous palette.
+#define COLOR_COUNT (4 * 3 + 1)
+#define BG_COLOR_INDEX (COLOR_COUNT - 1)
+static vgacolor old_palette[COLOR_COUNT];
+static uint8_t old_mode = 0;
 
-char old_palette[768];
-char vga_palette[768];
+// Macro to set a color palette entry
+#define _SETCOLOR(idx, r, g, b) outportb(0x3c8, idx); \
+    outportb(0x3c9, r); \
+    outportb(0x3c9, g); \
+    outportb(0x3c9, b);
 
-void v_read_palette (char *pal) {
+#define VGA_WIDTH 320
+#define VGA_HEIGHT 200
+
+// X and Y offsets to center the screen
+#define X_OFFSET ((VGA_WIDTH-LCD_WIDTH)/2)
+#define Y_OFFSET ((VGA_HEIGHT-LCD_HEIGHT)/2)
+
+static void v_dosvga_read_palette(vgacolor *pal, uint8_t index, int length) {
+    int i;
+    outportb(0x3c7, index); // Signal a read from VGA pal
+    for (i = 0; i < length; ++i) { // Read 3 color components at a time
+        pal->r = inportb(0x3c9);
+        pal->g = inportb(0x3c9);
+        pal->b = inportb(0x3c9);
+        pal++;
+    }
+}
+static void v_dosvga_write_palette(vgacolor *pal, uint8_t index, int length) {
+    int i;
+    outportb(0x3c8, index);
+    for (i = 0; i < length; ++i) {
+        outportb(0x3c9, pal->r);
+        outportb(0x3c9, pal->g);
+        outportb(0x3c9, pal++->b);
+    }
+}
 
 }
 
 int v_dosvga_init(video_config* cfg) {
+    uint8_t *screen = (uint8_t *) 0xa0000 + __djgpp_conventional_base;
+    __dpmi_regs regs;
 
-    // Set display to Mode 13h
-
+    // Enable near ptrs so we can write to conventional mem quickly
     if (__djgpp_nearptr_enable() == 0)
         return -1;
 
-    v_read_palette(old_palette);
+    // Read old palette so we can restore it afterwards
+    v_dosvga_read_palette(old_palette, 0, COLOR_COUNT);
 
-    __dpmi_regs regs;
+    // Save current video mode for later
+    memset(&regs, 0x00, sizeof regs);
+    regs.x.ax = 0x0F;
+    __dpmi_int(0x10, &regs);
 
-    memset(&regs, 0, sizeof regs);
+    old_mode = (uint8_t) regs.x.ax;
+
+
+    // Set display to Mode 13h
+    memset(&regs, 0x00, sizeof regs);
     regs.x.ax = 0x13;
     __dpmi_int(0x10, &regs);
+
+    // Make the background guaranteed black, set palette and clear the screen
+    _SETCOLOR(BG_COLOR_INDEX, 0x00, 0x00, 0x00);
+    memset(screen, BG_COLOR_INDEX, 320*200);
 
     return 0;
 }
 
 void v_dosvga_deinit() {
+    __dpmi_regs regs;
+
+    // Restore old palette & video mode
+
+    v_dosvga_write_palette(old_palette, 0, COLOR_COUNT);
+
+    memset(&regs, 0x00, sizeof regs);
+    regs.x.ax = old_mode;
+    __dpmi_int(0x10, &regs);
+
+    __djgpp_nearptr_disable();
+
+    printf("%s successful!\n", __func__);
 }
 
 static inline void v_dosvga_set_palette_rgb(uint8_t index, uint8_t color) {
     const vgacolor *new_color = &bw_palette[color];
-    outportb(0x3c8, index); /* want to write */
-    outportb(0x3c9, new_color->r);
-    outportb(0x3c9, new_color->g);
-    outportb(0x3c9, new_color->b);
+    _SETCOLOR(index, new_color->r, new_color->g, new_color->b);
 }
 
 void v_dosvga_update_palette(uint8_t pal_offset, gameboy_palette palette) {
@@ -78,13 +132,13 @@ void v_dosvga_update_palette(uint8_t pal_offset, gameboy_palette palette) {
 }
 
 void v_dosvga_write_line(int line, uint8_t *linebuf) {
-    uint8_t *screen = (uint8_t *) 0xa0000 + 320 * line + __djgpp_conventional_base;
+    // Center the screen
+    uint8_t *screen = (uint8_t *) 0xa0000 + __djgpp_conventional_base
+                        + 320 * (Y_OFFSET + line)
+                        + X_OFFSET;
+
+    // Since VGA paletting works like we do, this is a memcpy :-)
     memcpy(screen, linebuf, LCD_WIDTH);
-/*    int16_t i;
-    uint32_t *dst_line = &framebuffer_pixels[LCD_WIDTH * line];
-    for (i = 0; i < LCD_WIDTH; i++) {
-        *(dst_line++) = pal_rgb[*(linebuf++)];
-    }*/
 }
 
 void v_dosvga_frame_done() {
